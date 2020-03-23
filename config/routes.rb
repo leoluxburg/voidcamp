eb9c7450875227ea1992c93dc5483c7a8de1d11a
Leonardo Luxburg <leo.von.luxburg@gmail.com>
2020-03-22 19:18:46 -0500

    update

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
+  resources :lessons
+  resources :articles
   devise_for :users
   root to: 'pages#home'
+  get '/voidcamp', to: 'pages#voidcamp'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
